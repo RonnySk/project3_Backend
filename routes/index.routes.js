5fd61cd319b0a8ab04f3b5f551d8dd9054e87be7
Yvo de Rooij <110634893+YvodeRooij@users.noreply.github.com>
2023-07-18 10:05:33 +0200

    added chatbot backend

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const HousingProfile = require("../models/HousingProfile.model");
-
+const { Configuration, OpenAIApi } = require("openai");
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
@@ -15,6 +15,31 @@ router.post("/housingprofile", (req, res, next) => {
     .catch((err) => {
       res.status(500).json(err);
     });
+});
+
+// chatbot
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+router.post("/chatbot", async (req, res) => {
+  const userMessage = req.body.message;
+
+  try {
+    const chatResponse = await openai.createChatCompletion({
+      model: "gpt-4-0314",
+      messages: [
+        { role: "system", content: "You are consultant for people buying houses." },
+        { role: "user", content: userMessage },
+      ],
+    });
+
+    res.send(chatResponse.data.choices[0].message.content);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error processing the request");
+  }
 });
 
 module.exports = router;
